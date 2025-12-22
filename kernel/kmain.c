1fbf3b6c0c4090a18079e4f1b1bc064650db3f16
Swati Yelamanchili <swati.yelamanchili@gmail.com>
2025-12-22 20:30:00 +0530

    feat: wire GDT and IDT initialization into kernel boot sequence

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include "multiboot.h"
 
+extern void init_gdt(void);
+extern void init_idt(void);
+
 static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
@@ -19,5 +22,14 @@ void kernel_main(uint32_t magic, uint32_t addr) {
         vga[i] = (uint16_t)msg[i] | ((uint16_t)0x0A << 8);
     print_serial(msg);
     print_serial("VGA Memory mapped successfully.\n");
+
+    print_serial("Initializing GDT...\n");
+    init_gdt();
+    print_serial("Initializing IDT...\n");
+    init_idt();
+    print_serial("Enabling Interrupts...\n");
+    __asm__ volatile("sti");
+    print_serial("Kernel Ready.\n");
+
     while (1) __asm__ volatile("hlt");
 }
