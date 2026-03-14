f0bd27947d9a4368d4441d7b6708907bf5c9a1b7
Swati Yelamanchili <swati.yelamanchili@gmail.com>
2026-03-14 21:30:00 +0530

    test: add hello32 Ring 3 ELF test (infinite sys_write loop via int 0x80)

---
+++ tests/hello32.c
@@ -0,0 +1,15 @@
+void _start() {
+    while (1) {
+        const char *str = "Ring 3 Task Ping!\n";
+        __asm__ volatile (
+            "mov $4, %%eax\n"
+            "mov $1, %%ebx\n"
+            "mov %0, %%ecx\n"
+            "mov $18, %%edx\n"
+            "int $0x80\n"
+            :: "r"(str) : "eax", "ebx", "ecx", "edx"
+        );
+        
+        for (volatile int i = 0; i < 5000000; i++) {}
+    }
+}
